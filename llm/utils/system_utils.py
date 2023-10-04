84689a53b5646841c769457cb164d32f1ef6294d
Ayush Sawant <ayush.sawant@nutanix.com>
2023-10-05 00:38:53 +0530

    Removed no_generate,extra files, serialized files and changed download loaction of model files (#8)
    * removed no_generate,extra files, serialized files and changed download location of model files

--- llm/utils/system_utils.py
+++ llm/utils/system_utils.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import shutil
 
 def check_if_path_exists(filepath, param = ""):
     if not os.path.exists(filepath):
@@ -9,3 +10,12 @@ def check_if_path_exists(filepath, param = ""):
 def create_folder_if_not_exits(path):
     os.makedirs(path, exist_ok=True)
     print(f"The new directory is created! - {path}")
+
+def delete_directory(directory_path):
+    if not os.path.exists(directory_path):
+        return
+    try:
+        shutil.rmtree(directory_path)
+        print(f"Deleted all contents from '{directory_path}'")
+    except Exception as e:
+        print(f"Error deleting contents from '{directory_path}': {str(e)}")
