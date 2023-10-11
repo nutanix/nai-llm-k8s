58b48c7d50eb4a17493b44107a9539be178ef0fc
ayush <ayush.sawant@nutanix.com>
2023-10-11 07:43:27 +0000

    margen code cleanup

--- llm/utils/marsgen.py
+++ llm/utils/marsgen.py
@@ -28,14 +28,6 @@ def generate_mars(dl_model, model_config, model_store_dir, debug=False):
         extra_files_list = os.listdir(dl_model.model_path)
         extra_files_list = [os.path.join(dl_model.model_path, file) for file in extra_files_list]
         extra_files = ','.join(extra_files_list)
-        
-        runtime = None
-        if model.get("runtime") and model["runtime"]:
-            runtime = model["runtime"]
-
-        archive_format = None
-        if model.get("archive_format") and model["archive_format"]:
-            archive_format = model["archive_format"]
 
         requirements_file = None
         if model.get("requirements_file") and model["requirements_file"]:
@@ -43,21 +35,13 @@ def generate_mars(dl_model, model_config, model_store_dir, debug=False):
             check_if_path_exists(requirements_file)
 
         export_path = model_store_dir
-        if model.get("export_path") and model["export_path"]:
-            export_path = model["export_path"]
-            check_if_path_exists(export_path)
 
-        model_file_input = None
-        if model.get("model_file") and model["model_file"]:
-            model_file_input = model["model_file"]
-
-        cmd = model_archiver_command_builder(dl_model.model_name,
-                                             dl_model.repo_version,
-                                             model_file_input,
-                                             handler, extra_files,
-                                             runtime, archive_format, 
-                                             requirements_file,
-                                             export_path,
+        cmd = model_archiver_command_builder(model_name=dl_model.model_name,
+                                             version=dl_model.repo_version,
+                                             handler=handler, 
+                                             extra_files=extra_files, 
+                                             requirements_file=requirements_file,
+                                             export_path=export_path,
                                              debug=debug)
 
         debug and print(f"## In directory: {os.getcwd()} | Executing command: {cmd}\n")
